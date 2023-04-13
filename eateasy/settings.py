77a6fde58c5fc910bf2037b70d8582f94fd2217e
t-hullis <t.hullis@yahoo.com>
2023-04-13 17:29:51 +0000

    changed allowed host

--- eateasy/settings.py
+++ eateasy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['p-4-booking-system.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['p4-restaurant-booking-sys.herokuapp.com', 'localhost']
 
 
 # Application definition
