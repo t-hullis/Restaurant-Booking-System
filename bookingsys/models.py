75b045e2bdb5ed12543e26ce524b9e485cad842f
t-hullis <t.hullis@yahoo.com>
2023-06-14 16:21:10 +0000

    modify models.py to make booking page work

--- bookingsys/models.py
+++ bookingsys/models.py
@@ -1,16 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
-
-
-
-# Create your models here.
-class NewUser:
-    email = models.EmailField()
-    user_name = models.CharField(max_length=120)
-    first_name = models.CharField(max_length=120)
-    last_name = models.CharField(max_length=120)
-    
+from django.contrib.auth.models import User
 
 
 time_choices = (
