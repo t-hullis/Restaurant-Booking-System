156fdd1947bf3aaa1ba1bc5118b5aee2a6ee2a4e
t-hullis <t.hullis@yahoo.com>
2023-06-14 12:06:03 +0000

    add https://p4-restaurant-booking-sys.herokuapp.com/ to allowed_hosts

--- eateasy/settings.py
+++ eateasy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['p-4-booking-system.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['p-4-booking-system.herokuapp.com', 'https://p4-restaurant-booking-sys.herokuapp.com/', 'localhost']
 
 
 
